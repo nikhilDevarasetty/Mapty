f6fa7481ac20f1fb482d789e62ab2aa470aba688
nikhilDevarasetty <nicholus158@gmail.com>
2020-11-28 23:38:07 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import classes from "./style.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className={classes.sidebar}> </div>
+    </div>
+  );
+}
+
+export default App;
